46d7d9d9048b95ce2ba2cf966512bb1032dddcab
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-03-16 13:08:58 +0200

    Added logic for empty input
    Added logic for empty input

--- scripts.js
+++ scripts.js
@@ -3,7 +3,13 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+ 
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  //Check for empty values
+  if (!dividend || !divider){
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
 });
